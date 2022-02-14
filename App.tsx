b7b00a97511f58e8e223df50c5fca857e189bcb3
plusbeauxjours <plusbeauxjours@gmail.com>
2022-02-14 19:13:16 +0700

    hello world

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { View, StatusBar, StyleSheet } from "react-native";
+import { View, StatusBar, StyleSheet, Dimensions } from "react-native";
+
+import { Canvas, Circle, Group } from "@shopify/react-native-skia";
 
 interface IProps {}
 const App: React.FC<IProps> = () => {
+  const center = {
+    x: Dimensions.get("window").width / 2,
+    y: Dimensions.get("window").height / 2,
+  };
+  const r = 25;
+
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={"light-content"} />
-    </View>
+    <Canvas style={{ flex: 1 }}>
+      <Circle c={center} r={r} color="tomato" />
+    </Canvas>
   );
 };
 
